7c4a50b2cab10cbb3d1a44eb3281c05c86709ecd
vivekpaliwal0405 <vivekpaliwal0011@gmail.com>
2024-09-05 19:13:46 +0530

    login stepup

--- app/login/page.js
+++ app/login/page.js
@@ -1,8 +1,14 @@
 "use client"
 import React from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
+import { useRouter } from 'next/navigation'
 
 const Login = () => {
+    const { data: session } = useSession()
+    if(session) {
+    const router = useRouter()
+    router.push("/dashboard")
+  }
   return (
  <div className='text-white py-14 container mx-auto'>
  <h1 className='text-center font-bold text-3xl'>Login to get your fans to support you</h1>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,12 @@
 "use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
 
 const Navbar = () => {
   const { data: session } = useSession()
-  if(session) {
-    return <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
-  }
+  const [showdropdown ,setshowdropdown] = useState(false)
+  
   return (
     <nav className='bg-black text-white flex justify-between px-4 h-16 items-center'>
       <div className='logo font-bold'>Get me a coofeee</div>
@@ -21,9 +17,38 @@ const Navbar = () => {
         <li>SIgn up</li>
         <li>Login</li>
       </ul> */}
-      <div>
+
+      <div className='relative'>
+        {session && <>
+      <button onClick={()=>setshowdropdown(!showdropdown)} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm mx-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Welcome {session.user.email} <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokewidth="2" d="m1 1 4 4 4-4"/>
+        </svg>
+      </button>
+      <div id="dropdown" className={`z-10 ${showdropdown?"":"hidden"} absolute left-[125px] bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+      <li>
+        <Link href="/dashboard" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</Link>
+      </li>
+      <li>
+        <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</Link>
+      </li>
+      <li>
+        <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</Link>
+      </li>
+      <li>
+        <Link href="#" onClick={()=>signOut()} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</Link>
+      </li>
+    </ul>
+      </div>
+      </>
+      }
+
+        
+        {session &&  <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={()=>{signOut()}}>Logout</button>}
+
+        {!session && 
         <Link href={"/login"}>
-        <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button></Link>
+        <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button></Link>}
       </div>
     </nav>
   )
